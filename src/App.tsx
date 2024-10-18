e9417a7f8dd047f8cd764b40ce2a0e336c5c5d9c
Ace Lawrence Zuñiga Clavano <92563567+Aces007@users.noreply.github.com>
2024-10-18 10:59:19 +0800

    added homepage

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import './index.css';
+import './App.scss';
+import Header from "./components/Homepage/Header.tsx"
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default App;
