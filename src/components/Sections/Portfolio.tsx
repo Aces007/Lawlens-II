daf1bd774fdf4d8eeb2d69113c3f2d4343d022e0
Ace Lawrence Zuñiga Clavano <92563567+Aces007@users.noreply.github.com>
2024-11-02 20:33:56 +0800

    finished portfolio section

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -3,24 +3,8 @@ import '../../App.scss';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
-import aceV1 from '../../assets/projects/Ace Software.png';
-import aceV2 from '../../assets/projects/Ace Software II.png';
-import aceV3 from '../../assets/projects/Ace Software (Using WordPress).png';
-import aceV4 from '../../assets/projects/Ace Software (Using WordPress) II.png';
-import echonox from '../../assets/projects/Echonox.png';
-import echonoxII from '../../assets/projects/Echonox II.png';
-import HWI from '../../assets/projects/HW Sugar I.jpg';
-import HWII from '../../assets/projects/HW Sugar II.jpg';
-import HWIII from '../../assets/projects/HW Sugar III.jpg';
-import embark from '../../assets/projects/Embark.png';
-import embarkII from '../../assets/projects/Embark II.png';
-import lawlensI from '../../assets/projects/Lawlens I.png';
-import lawlensII from '../../assets/projects/Lawlens II.png';
-import revizio from '../../assets/projects/Revizio I.png';
-import revizioII from '../../assets/projects/Revizio II.png';
-import revizioIII from '../../assets/projects/Revizio III.png';
-import revizioIV from '../../assets/projects/Revizio IV.png';
-
+import {images} from './projectImages.ts';
+import {multimedia} from './projectImages.ts';
 
 
 const Portfolio = () => {
@@ -29,7 +13,7 @@ const Portfolio = () => {
             projName: "Ace Software V1",
             startDate: "April 2023",
             projBase: "Vanilla HTML/CSS",
-            projImage: [aceV1, aceV2],
+            projImage: [images.aceV1, images.aceV2],
             projOrient: "landscape",
             projTags : ["Vanilla", "UI/UX Design", "Personal Website"]
         },
@@ -37,7 +21,7 @@ const Portfolio = () => {
             projName: "Ace Software V2",
             startDate: "September 2023",
             projBase: "Wordpress",
-            projImage: [aceV3, aceV4],
+            projImage: [images.aceV3, images.aceV4],
             projOrient: "landscape",
             projTags : ["Wordpress", "UI/UX Design", "Personal Website"]
         },
@@ -45,7 +29,7 @@ const Portfolio = () => {
             projName: "Echonox",
             startDate: "April 2024",
             projBase: "Vanilla HTML/CSS",
-            projImage: [echonox, echonoxII],
+            projImage: [images.echonox, images.echonoxII],
             projOrient: "landscape",
             projTags : ["Vanilla", "Telecall Web Dev Program", "Creative Web Design Output"]
         },
@@ -53,7 +37,7 @@ const Portfolio = () => {
             projName: "Hardware Sugar",
             startDate: "September 2023",
             projBase: "Figma",
-            projImage: [HWI, HWII, HWIII],
+            projImage: [images.HWI, images.HWII, images.HWIII],
             projOrient: "portrait",
             projTags : ["Figma", "UI/UX Design", "Website Reimagination"]
         },
@@ -61,7 +45,7 @@ const Portfolio = () => {
             projName: "Embark",
             startDate: "May 2024",
             projBase: "Vanilla HTML/CSS",
-            projImage: [embark, embarkII],
+            projImage: [images.embark, images.embarkII],
             projOrient: "landscape",
             projTags : ["Vanilla", "Telecall Web Dev Program", "Creative Web Design Output"]
         },
@@ -69,7 +53,7 @@ const Portfolio = () => {
             projName: "Lawlens V1",
             startDate: "June 2024",
             projBase: "Vanilla HTML/CSS",
-            projImage: [lawlensI, lawlensII],
+            projImage: [images.lawlensI, images.lawlensII],
             projOrient: "landscape",
             projTags : ["Vanilla", "UI/UX Design", "Figma Design", "Personal Website Recreation"]
         },
@@ -77,23 +61,10 @@ const Portfolio = () => {
             projName: "Revizio",
             startDate: "September 2024",
             projBase: "Figma",
-            projImage: [revizio, revizioII, revizioIII, revizioIV],
+            projImage: [images.revizio, images.revizioII, images.revizioIII, images.revizioIV],
             projOrient: "portrait",
             projTags : ["Figma", "UI/UX Design", "System Development", "App Dev"]
         },
-    ]
-
-    const multimedia = [
-        {
-            platform: "IG",
-            projName: "Digital Dreamscapes",
-            startDate: "Since 2023",
-            projBase: "AI & PS"
-        },
-        {
-            platform: "YT"
-        },
-
     ]
 
     const sliderSettings = {
@@ -103,6 +74,32 @@ const Portfolio = () => {
         slidesToShow: 1,
         slidesToScroll: 1,
     }
+
+    const instagramPhotos = [
+        multimedia.blake,
+        multimedia.curves,
+        multimedia.happy,
+        multimedia.hoodie,
+        multimedia.mgs,
+        multimedia.mgs2,
+        multimedia.mountains,
+        multimedia.paupau,
+        multimedia.orton
+    ]
+
+    const ytVids = [
+        "https://www.youtube.com/embed/Bj-ZzEgTJhE?si=t-WKbI5ul1XTHPZ-",
+        "https://www.youtube.com/embed/Av1Z56sr6R4?si=mvW8NdtMSO72NNZ0",
+        "https://www.youtube.com/embed/J_-4MxjTggo?si=26xecrSvH9dpx0_R",
+        "https://www.youtube.com/embed/jVWHjrIJZcQ?si=n59tNwcu4_I5pNhL",
+        "https://www.youtube.com/embed/vA0-u_J7rus?si=auz2rmujEMIIo_R_",
+        "https://www.youtube.com/embed/KRm_mUcPMwk?si=YNoY5CM5F95P3wdv",
+        "https://www.youtube.com/embed/gIvG4sXVg8g?si=4PX-htQf9f7qTD_b",
+        "https://www.youtube.com/embed/gP4FD7TghLA?si=ceeez-SIQexM4k8I",
+        "https://www.youtube.com/embed/pd8q1HT6-tw?si=kKgZpv-4ABddb6tZ",
+        "https://www.youtube.com/embed/dJ_pnfoZ-GM?si=tNLJBQ59LMwvieei",
+        "https://www.youtube.com/embed/bQ7Dkoy2hLA?si=VjuHcdZZ0Ilqt0Sx",
+    ]
 
     return (
         <>
@@ -136,15 +133,41 @@ const Portfolio = () => {
                     </div>
                 ))}
 
-                {multimedia.map((multi, index) => (
-                    <div key={index} className='multimedia_content'>
-                        <h2 className="projName">{multi.projName}</h2>
-                        <div className="project_info">
-                            <p className="startDate">{multi.startDate}</p>
-                            <p className="projBase">{multi.projBase}</p>
+                <div className="multi_content">
+                    <div className="ig_content">
+                        <h2 id="multi_label">Digi Dreamscapes</h2>
+                        <div className="multi_content_info">
+                            <p className="startDate">Since 2022</p>
+                            <p className="projBase">Photography</p>
                         </div>
+                        <Slider {...sliderSettings} className='multi_slider'>
+                            {instagramPhotos.map((post, index) => (
+                                <img key={index} src={post} className='multi_embed'></img>
+                            ))}
+                        </Slider>
                     </div>
-                ))}
+
+                    <div className="yt_content">
+                        <h2 id="multi_label">Art of Film</h2>
+                        <div className="multi_content_info">
+                            <p className="startDate">Since 2022</p>
+                            <p className="projBase">YouTube</p>
+                        </div>
+                        <Slider {...sliderSettings} className='multi_slider'>
+                            {ytVids.map((ytPost, ytIndex) => (
+                                <iframe
+                                    key={ytIndex}
+                                    src={ytPost}
+                                    width="100%"
+                                    height="3   15"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen
+                                    className="video_frame"
+                                ></iframe>
+                            ))}
+                        </Slider>
+                    </div>
+                </div>
             </div>  
         </>
     )
